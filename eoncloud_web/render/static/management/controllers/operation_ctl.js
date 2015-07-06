6cb498fcf84ff4eb5fea1ea04173b4ead6fa80d4
bluven <yanshiyi1983@gmail.com>
2015-07-06 16:03:25 +0800

    Add filter function to Operation Management.

--- eoncloud_web/render/static/management/controllers/operation_ctl.js
+++ eoncloud_web/render/static/management/controllers/operation_ctl.js
@@ -8,15 +8,35 @@ CloudApp.controller('OperationController', function($rootScope, $scope, $filter,
     $rootScope.settings.layout.pageBodySolid = true;
     $rootScope.settings.layout.pageSidebarClosed = false;
 
+    $scope.filterEnabled = true;
+
+    $scope.toggleFilter = function(){
+        $scope.filterEnabled = !$scope.filterEnabled;
+    };
+
     $scope.operation_table = new ngTableParams({
         page: 1,
-        count: 10
+        count: 10,
+        filter: {
+            resource_i18n: '',
+            operator: '',
+            data_center_name: ''
+        }
     },{
         counts: [],
         getData: function ($defer, params) {
             Operation.query(function (data) {
-                var data_list = params.sorting() ?
-                    $filter('orderBy')(data, params.orderBy()) : volume.name;
+
+                var data_list =  data;
+
+                if(params.filter()){
+                    data_list = $filter('filter')(data_list, params.filter());
+                }
+
+                if (params.sorting()){
+                    data_list = $filter('orderBy')(data_list, params.orderBy());
+                }
+
                 params.total(data_list.length);
                 $defer.resolve(data_list.slice((params.page() - 1) * params.count(), params.page() * params.count()));
             });
